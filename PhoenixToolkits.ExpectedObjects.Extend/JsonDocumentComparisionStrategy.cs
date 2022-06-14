abfa525b472c665d359b5ade63d4c6944786e1d2
Phoenix <phoenix.chen@rayprosoft.com>
2022-06-14 11:59:34 +0800

    加入JsonDocumentComparisionStrategy

---
+++ PhoenixToolkits.ExpectedObjects.Extend/JsonDocumentComparisionStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using ExpectedObjects.Reporting;
+using ExpectedObjects.Strategies;
+
+namespace ExpectedObjects
+{
+	public class JsonDocumentComparisionStrategy : IComparisonStrategy
+	{
+		public bool CanCompare(object expected, object actual) => expected is JsonDocument;
+
+		public bool AreEqual(object expected, object actual, IComparisonContext comparisonContext)
+			=> JsonSerializer.Serialize(expected)
+				.ToExpectedObject()
+				.Equals(JsonSerializer.Serialize(actual), NullWriter.Instance, false);
+	}
+}
